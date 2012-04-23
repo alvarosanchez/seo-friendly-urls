5f2b5c9d014677edddd5fbdf5ee5195355319377
Alvaro Sanchez-Mariscal <alvaro.sanchez@salenda.es>
2012-04-23 17:45:21 +0200

    Filled plugin data

--- SeoFriendlyUrlsGrailsPlugin.groovy
+++ SeoFriendlyUrlsGrailsPlugin.groovy
@@ -1,8 +1,8 @@
 class SeoFriendlyUrlsGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "1.0.0.M1"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.0 > *"
+    def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
@@ -11,11 +11,13 @@ class SeoFriendlyUrlsGrailsPlugin {
     ]
 
     // TODO Fill in these fields
-    def title = "Seo Friendly Urls Plugin" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
+    def title = "Seo Friendly URL's Plugin" // Headline display name of the plugin
+    def author = "Álvaro Sánchez-Mariscal"
+    def authorEmail = "alvaro.sanchez@salenda.es"
     def description = '''\
-Brief summary/description of the plugin.
+Helps to easily convert any string into a SEO-friendly one, eg from "The Lord of the Rings" to "the-lord-of-the-rings".
+
+Useful if you want SEO-friendly URL's like /book/the-lord-of-the-rings instead of /book/show/123.
 '''
 
     // URL to the plugin's documentation
@@ -24,19 +26,21 @@ Brief summary/description of the plugin.
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+    def organization = [ name: "Salenda", url: "http://www.salenda.es" ]
 
     // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+    def developers = [ 
+		[ name: "Jesús Lanchas", email: "jesus.lanchas@daureos.com" ]
+	]
 
     // Location of the plugin's issue tracker.
 //    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
+    def scm = [ url: "http://github.com/alvarosanchez/seo-friendly-urls" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
