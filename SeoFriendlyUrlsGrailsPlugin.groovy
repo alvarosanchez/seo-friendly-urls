d971505af333271761b928496c0d0671ed305593
Alvaro Sanchez-Mariscal <alvaro.sanchez@salenda.es>
2012-04-23 20:39:15 +0200

    It can be considered final

--- SeoFriendlyUrlsGrailsPlugin.groovy
+++ SeoFriendlyUrlsGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class SeoFriendlyUrlsGrailsPlugin {
     // the plugin version
-    def version = "1.0.0.M1"
+    def version = "1.0.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3 > *"
     // the other plugins this plugin depends on
